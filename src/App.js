162bffd53e928658315a99ca4acdcdbaad37f265
Everett Bazzocchi <contact@everettbazzocchi.ca>
2024-04-17 17:11:28 -0500

    Header Design Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+
+//import Dashboard from './components/Dashboard/Dashboard';
+//import Auth from './components/Auth/Auth';
+import IMS from './components/ims/IMS';
+import Home from './components/Home/Home';
+//import Settings from './components/Settings/Settings';
+//import POS from './components/POS/POS';
+
+import HeaderComp from './components/Header/HeaderComp';
+import FooterComp from './components/Footer/FooterComp';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <HeaderComp />
+            <Router>
+                <Switch>
+                    <Route path='/ims' element={<IMS />} />
+                    <Route path='/' element={<Home />} />
+                </Switch>
+            </Router>
+            <FooterComp />
+
+        </ThemeProvider>
+
+    );
 }
 
 export default App;
